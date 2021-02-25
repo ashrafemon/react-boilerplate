10d4b31a5d14e8a9629aa2950f880ba05753180e
Ashraf Emon <ashraf.emon143@gmail.com>
2021-02-26 03:38:52 +0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Suspense} from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import routes from './routes'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                    {routes.map((item, index) => (
+                        <Route key={index} exact={item.exact} path={item.path} component={item.component}/>
+                    ))}
+                </Switch>
+            </Suspense>
+        </BrowserRouter>
+    );
 }
 
 export default App;
